823360d93784a02ea278df0756e7e3fb09937cf9
lsc949982212 <949982212@qq.com>
2020-06-12 15:43:47 +0800

    directory

--- backend/schema/service_config.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose')
-const serviceConfig = new mongoose.Schema({
-	height:Number
-})
-let serviceConfigModel = mongoose.model('service_config',serviceConfig,'service_config')
-module.exports = serviceConfigModel
