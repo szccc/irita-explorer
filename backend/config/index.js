b74a9d51b5690f4a75648b5da63ff401ee00f63d
hangTaishan <hangTaishan@bishuilantiandeMacBook-Pro.local>
2020-08-28 10:40:09 +0800

    #

--- backend/config/index.js
+++
@@ -1,8 +0,0 @@
-const configuration = require("./config");
-
-const config = {
-	mongoUrl: configuration.app.mongoUrl,
-	lcdAddress:configuration.app.lcdAddress,
-	syncServiceTime: configuration.app.syncServiceTime
-};
-module.exports = config;
