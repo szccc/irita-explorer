969b147da1667f3df436db79baabbb49292b6f20
duanjie_cmd <18870420982@139.com>
2020-11-05 11:31:01 +0800

    bug fix

--- src/components/TxListPage.vue
+++ src/components/TxListPage.vue
@@ -199,7 +199,7 @@
 				this.currentPageNum = 1;
 				sessionStorage.setItem('txpagenum', 1);
 				history.pushState(null, null, `/#${this.$route.path}?txType=${this.TxType}&status=${this.txStatus}&startTime=${this.urlParamsShowStartTime}&endTime=${this.urlParamsShowEndTime}&page=1`);
-				this.getTxListByFilterCondition();
+				this.getTxListByFilterCondition();		
 			},
 			resetUrl () {
 				this.value = 'allTxType';
@@ -393,31 +393,35 @@
 							if (res.data) {
 								this.txList = []
 								res.data.forEach(async (item) => {
-									let msgsNumber = item.msgs ? item.msgs.length : 0
-									const fee = item.fee && item.fee.amount && item.fee.amount.length > 0 ? await converCoin(item.fee.amount[0]) :'--'
-									const time = Tools.getDisplayDate(item.time)
-									// let OperatorAddr = item.msgs && item.msgs.length === 1 ? item.msgs[0].msg && item.msgs[0].msg.validator_address ? item.msgs[0].msg.validator_address : '--' : '--'
-									let OperatorAddr = item.msgs && item.msgs.length === 1 ? item.msgs[0] && TxHelper.getValidationTxsOperator(item.msgs[0]) : '--'
-									let OperatorMonikers
-									if(item.monikers.length) {
-										item.monikers.map( it => {
-											OperatorMonikers = OperatorMonikers || it[OperatorAddr] || ''
-										})
-									}
-									this.txList.push({
-											Tx_Hash: item.tx_hash,
-											Block: item.height,
-											// Moniker: item.msgs && item.msgs.length === 1 ? item.msgs[0].msg && item.msgs[0].msg.description && item.msgs[0].msg.description.moniker ? item.msgs[0].msg.description && item.msgs[0].msg.description.moniker : '--' : '--',
-											OperatorAddr,
-											OperatorMonikers: OperatorMonikers || '--',
-											SelfBonded: item.msgs && item.msgs.length === 1 ? item.msgs[0].msg && item.msgs[0].msg.min_self_delegation ? `${item.msgs[0].msg.min_self_delegation} ${mainToken.symbol.toUpperCase()}` : '--' : '--',
-											'Tx_Type': (item.msgs || []).map(item=>item.type),
-											MsgsNum: msgsNumber,
-											'Tx_Fee': fee && fee.amount ? `${Tools.formatPriceToFixed(fee.amount,this.amountDecimals)} ${fee.denom.toLocaleUpperCase()}` : '--',
-											'Tx_Signer': item.signers[0] ? item.signers[0] : '--',
-											'Tx_Status': TxStatus[item.status],
-											Timestamp: time,
-									})
+									setTimeout(async()=> {
+										if(item) {
+											let msgsNumber = item.msgs ? item.msgs.length : 0
+											const fee = item.fee && item.fee.amount && item.fee.amount.length > 0 ? await converCoin(item.fee.amount[0]) :'--'
+											const time = Tools.getDisplayDate(item.time)
+											// let OperatorAddr = item.msgs && item.msgs.length === 1 ? item.msgs[0].msg && item.msgs[0].msg.validator_address ? item.msgs[0].msg.validator_address : '--' : '--'
+											let OperatorAddr = item.msgs && item.msgs.length === 1 ? item.msgs[0] && TxHelper.getValidationTxsOperator(item.msgs[0]) : '--'
+											let OperatorMonikers
+											if(item.monikers.length) {
+												item.monikers.map( it => {
+													OperatorMonikers = OperatorMonikers || it[OperatorAddr] || ''
+												})
+											}
+											this.txList.push({
+													Tx_Hash: item.tx_hash,
+													Block: item.height,
+													// Moniker: item.msgs && item.msgs.length === 1 ? item.msgs[0].msg && item.msgs[0].msg.description && item.msgs[0].msg.description.moniker ? item.msgs[0].msg.description && item.msgs[0].msg.description.moniker : '--' : '--',
+													OperatorAddr,
+													OperatorMonikers: OperatorMonikers || '--',
+													SelfBonded: item.msgs && item.msgs.length === 1 ? item.msgs[0].msg && item.msgs[0].msg.min_self_delegation ? `${item.msgs[0].msg.min_self_delegation} ${mainToken.symbol.toUpperCase()}` : '--' : '--',
+													'Tx_Type': (item.msgs || []).map(item=>item.type),
+													MsgsNum: msgsNumber,
+													'Tx_Fee': fee && fee.amount ? `${Tools.formatPriceToFixed(fee.amount,this.amountDecimals)} ${fee.denom.toLocaleUpperCase()}` : '--',
+													'Tx_Signer': item.signers[0] ? item.signers[0] : '--',
+													'Tx_Status': TxStatus[item.status],
+													Timestamp: time,
+											})
+										}
+									},0)
 								})
 							} else {
 								this.txList = [];
@@ -447,7 +451,7 @@
 				if (!moniker) {
 					return ''
 				}
-				return Tools.formatString(moniker, 15, '...')
+				return Tools.formatString(moniker, 8, '...')
 			},
 			getDisplayTxType(types=[]){
 				let type = types[0] || '';
