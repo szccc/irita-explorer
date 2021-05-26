d0afd656cbb0ba39524d8d152eee6d6f4972c112
段劼 <65702143+duanjie-cmd@users.noreply.github.com>
2021-05-26 14:41:42 +0800

    todo tx detail WithdrawDelegatorReward

--- src/components/common/TxMessage.vue
+++ src/components/common/TxMessage.vue
@@ -2552,7 +2552,7 @@
 								break;
 							case TX_TYPE.withdraw_delegator_reward:
 								this.from = msg.validator_address;
-								// this.to = msg.delegator_address;
+								this.to = msg.delegator_address;
 								;(this.events || []).forEach((item) => {
 									if(item.type === 'withdraw_rewards') {
 										let isAmount = (item.attributes || []).some(item => {
@@ -2567,23 +2567,23 @@
 										}
 									}
 								});
-								(this.events || []).forEach(item => {
-									if(item.type === 'transfer') {
-										let isTransfer = (item.attributes || []).some(item => {
-											return item.value == amount
-										})
-										if(isTransfer) {
-											(item.attributes || []).forEach((attr) => {
-												if (attr.key == 'recipient') {
-													this.to = attr.value || '--';
-												}
-											});
-										}
-									}
-								})
-								if(!this.to) {
-									this.to = '--'
-								}
+								// (this.events || []).forEach(item => {
+								// 	if(item.type === 'transfer') {
+								// 		let isTransfer = (item.attributes || []).some(item => {
+								// 			return item.value == amount
+								// 		})
+								// 		if(isTransfer) {
+								// 			(item.attributes || []).forEach((attr) => {
+								// 				if (attr.key == 'recipient') {
+								// 					this.to = attr.value || '--';
+								// 				}
+								// 			});
+								// 		}
+								// 	}
+								// })
+								// if(!this.to) {
+								// 	this.to = '--'
+								// }
 								if( amount && amount !== '--') {
 									amount = await converCoin(amount);
 									this.amount = `${amount.amount} ${amount.denom.toUpperCase()}`;
